c96d7b66580991345d6649b3a9236e8e5d4917c3
RicardoTorresits <ricardo.torresits@gmail.com>
2025-03-06 17:24:01 -0600

    base para la conecion de bd

---
+++ JavaWebCrud/src/main/java/com/mycompany/models/Persona.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.models;
+
+/**
+ *
+ * @author it-seekers
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private int Edad;
+    private String correo;
+
+    public Persona(){}
+    
+    public Persona(String nombre, int Edad, String correo) {
+        this.nombre = nombre;
+        this.Edad = Edad;
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
+}
